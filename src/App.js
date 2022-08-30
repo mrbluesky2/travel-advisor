b9ad15b7c704a5f20429b0146ac6b5a32f87a500
mrbluesky2 <pierrecarr2@gmail.com>
2022-08-30 11:53:20 -0700

    restaurants API and basic UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import { getPlacesData } from './api';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
 
 function App () {
+    const [places, setPlaces] = useState([]);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState(null);
+
+    useEffect(() => {
+        // get the user's location on startup
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude }}) => {
+            setCoordinates({ lat: latitude, lng: longitude });
+        });
+    }, []);
+
+    useEffect(() => {
+        // get the places when the map changes
+        if (bounds) {
+            getPlacesData(bounds.sw, bounds.ne).then((data) => {
+                console.log(data);
+                setPlaces(data);
+            });
+        }
+    }, [bounds]);
+
     return (
-        <h1>Hello, World!</h1>
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                <Grid item xs={12} md={4}>
+                    <List places={places} />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                    />
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
---
+++ src/api/index.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async (sw, ne) => {
+    try {
+        const { data: { data } } = await axios.get(URL, {
+            params: {
+                bl_latitude: sw.lat,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng,
+                tr_longitude: ne.lng,
+            },
+            headers: {
+                'X-RapidAPI-Key': '0e0cb9b0d1msh48ee690c8b99110p159e04jsn410f9441958b',
+                'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+            }
+        });
+        
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
